bd19a98e46c2667c7d5db7ca93d68f5b1fbac4a1
sayemp2 <sojibulislamsayem420@gmail.com>
2024-03-16 01:36:23 +0600

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-5xl bg-black text-white">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
